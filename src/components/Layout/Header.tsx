37179747552510b12c39a3e75a5d2fe7c47a1941
Tarwat <t.uddin9121@gmail.com>
2022-06-26 18:34:04 +0100

    Update Header component
    Added styling and links to relevent routes

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from "gatsby";
+import { Link as RouterLink } from "gatsby";
+import { Stack, Link } from "@mui/material";
 
 type Props = { siteTitle: string };
 
 export default function Header({ siteTitle }: Props) {
   return (
     <header>
-      <Link to="/">{siteTitle || "Index"}</Link>
+      <Stack component="nav" direction="row" spacing={1}>
+        <Link component={RouterLink} to="/">
+          {siteTitle || "Homepage"}
+        </Link>
+        <Link component={RouterLink} to="/blogs/latest">
+          Latest
+        </Link>
+      </Stack>
     </header>
   );
 }
