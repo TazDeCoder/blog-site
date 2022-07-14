a2547affe54855b95806a7e0be527ab8ac31edd4
Tarwat <t.uddin9121@gmail.com>
2022-07-14 14:58:11 +0100

    Update gatsby-node file
    Added hotfix for issue with page delays and removed unnecessary logic

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -21,37 +21,30 @@ export const createPages: GatsbyNode["createPages"] = async ({
   `);
 
   const { createPage } = actions;
-  if (data.allStrapiArticle?.nodes) {
-    // LATEST BLOG-POST PAGES
-    data.allStrapiArticle.nodes.map((node: { slug: string }) => {
+  // LATEST BLOG-POST PAGES
+  data.allStrapiArticle.nodes.map((node: { slug: string }) => {
+    createPage({
+      path: `/blogs/latest/${node.slug}`,
+      component: path.resolve("./src/templates/blog-post.tsx"),
+      context: { slug: node.slug },
+    });
+  });
+  // SINGLE-CATEGORY PAGES
+  data.allStrapiCategory.nodes.map((node: { slug: string }) => {
+    createPage({
+      path: `/blogs/tags/${node.slug}`,
+      component: path.resolve("./src/templates/category.tsx"),
+      context: { slug: node.slug },
+    });
+  });
+  // CATEGORY BLOG-POST PAGES
+  data.allStrapiCategory.nodes.map((catg: { slug: string }) => {
+    data.allStrapiArticle.nodes.map((post: { slug: string }) => {
       createPage({
-        path: `/blogs/latest/${node.slug}`,
+        path: `/blogs/tags/${catg.slug}/${post.slug}`,
         component: path.resolve("./src/templates/blog-post.tsx"),
-        context: { slug: node.slug },
+        context: { slug: post.slug },
       });
     });
-  }
-  if (data.allStrapiCategory?.nodes) {
-    // SINGLE-CATEGORY PAGES
-    data.allStrapiCategory.nodes.map((node: { slug: string }) => {
-      createPage({
-        path: `/blogs/tags/${node.slug}`,
-        component: path.resolve("./src/templates/category.tsx"),
-        context: { slug: node.slug },
-      });
-    });
-  }
-  if (data.allStrapiCategory?.nodes && data.allStrapiArticle?.nodes) {
-    // CATEGORY BLOG-POST PAGES
-    data.allStrapiCategory.nodes.map((catg: { slug: string }) => {
-      data.allStrapiArticle.nodes.map((post: { slug: string }) => {
-        createPage({
-          path: `/blogs/tags/${catg.slug}/${post.slug}`,
-          component: path.resolve("./src/templates/blog-post.tsx"),
-          context: { slug: post.slug },
-          defer: true,
-        });
-      });
-    });
-  }
+  });
 };
